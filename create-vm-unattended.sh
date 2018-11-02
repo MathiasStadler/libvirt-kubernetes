19693b8f8eb1c7f6663b2e52ef3bffda72e765b4
MathiasStadler <email@mathias-stadler.de>
2018-11-02 15:42:44 +0100

    add unattenden

---
+++ create-vm-unattended.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "$1" ] ;
+then
+ echo Specify a virtual-machine name.
+ exit 1
+fi
+
+sudo virt-install \
+--name $1 \
+--ram 4096 \
+--disk path=/var/lib/libvirt/images/$1.img,size=30 \
+--vcpus 2 \
+--os-type linux \
+--os-variant ubuntu16.04 \
+--network bridge:br0,model=virtio \
+--graphics none \
+--console pty,target_type=serial \
+--location  /home/trapapa/playground/linux-unattended-installation/ubuntu-16.04-netboot-amd64-unattended.iso \
+--extra-args 'console=ttyS0,115200n8 serial'
